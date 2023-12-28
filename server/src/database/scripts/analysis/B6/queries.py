f384b9879f6fc3f080b6058e059e96f0817287e1
Omar Bouattour <omar.bouattour@yahoo.fr>
2023-12-28 18:16:31 +0100

    http analysis anfragen

--- server/src/database/scripts/analysis/B6/queries.py
+++ server/src/database/scripts/analysis/B6/queries.py
@@ -7,62 +7,114 @@ from src.database.database import get_db
 
 
 def q1():
-    with open('q1.sql', 'r') as file:
+    with open('/database/scripts/analysis/B6/q1.sql', 'r') as file:
         query = file.read()
 
     with get_db() as db:
         result = db.execute(text(query))
-        return result.fetchall()
+
+        # Fetch column names
+        column_names = result.keys()
+
+        # Fetch all rows and convert them to dictionaries
+        rows = result.fetchall()
+        result_list = [dict(zip(column_names, row)) for row in rows]
+
+    return result_list
 
 def q2():
-    with open('q2.sql', 'r') as file:
+    with open('/database/scripts/analysis/B6/q2.sql', 'r') as file:
         query = file.read()
 
     with get_db() as db:
         result = db.execute(text(query))
-        return result.fetchall()
+
+        # Fetch column names
+        column_names = result.keys()
+
+        # Fetch all rows and convert them to dictionaries
+        rows = result.fetchall()
+        result_list = [dict(zip(column_names, row)) for row in rows]
+    return result_list
+
 def q3():
-    with open('q3.sql', 'r') as file:
+    with open('/database/scripts/analysis/B6/q3.sql', 'r') as file:
         query = file.read()
 
     with get_db() as db:
         result = db.execute(text(query))
-        return result.fetchall()
+
+        # Fetch column names
+        column_names = result.keys()
+
+        # Fetch all rows and convert them to dictionaries
+        rows = result.fetchall()
+        result_list = [dict(zip(column_names, row)) for row in rows]
+    return result_list
 
 
 def q4():
-    with open('q4.sql', 'r') as file:
+    with open('/database/scripts/analysis/B6/q4.sql', 'r') as file:
         query = file.read()
 
     with get_db() as db:
         result = db.execute(text(query))
-        return result.fetchall()
+
+        # Fetch column names
+        column_names = result.keys()
+
+        # Fetch all rows and convert them to dictionaries
+        rows = result.fetchall()
+        result_list = [dict(zip(column_names, row)) for row in rows]
+    return result_list
 
 def q5():
-    with open('q5.sql', 'r') as file:
+    with open('/database/scripts/analysis/B6/q5.sql', 'r') as file:
         query = file.read()
 
     with get_db() as db:
         result = db.execute(text(query))
-        return result.fetchall()
+
+        # Fetch column names
+        column_names = result.keys()
+
+        # Fetch all rows and convert them to dictionaries
+        rows = result.fetchall()
+        result_list = [dict(zip(column_names, row)) for row in rows]
+    return result_list
 def q6_losers():
-    with open('q6-losers.sql', 'r') as file:
+    with open('/database/scripts/analysis/B6/q6-losers.sql', 'r') as file:
         query = file.read()
 
     with get_db() as db:
         result = db.execute(text(query))
-        return result.fetchall()
+
+        # Fetch column names
+        column_names = result.keys()
+
+        # Fetch all rows and convert them to dictionaries
+        rows = result.fetchall()
+        result_list = [dict(zip(column_names, row)) for row in rows]
+    return result_list
 
 def q6_winners():
-    with open('q6-winners.sql', 'r') as file:
+    with open('/database/scripts/analysis/B6/q6-winners.sql', 'r') as file:
         query = file.read()
 
     with get_db() as db:
         result = db.execute(text(query))
-        return result.fetchall()
+
+        # Fetch column names
+        column_names = result.keys()
+
+        # Fetch all rows and convert them to dictionaries
+        rows = result.fetchall()
+        result_list = [dict(zip(column_names, row)) for row in rows]
+    return result_list
 
 
 if __name__ == "__main__":
-    result_query = q6_winners()
+    result_query = q1()
+    print(result_query)
     for row in result_query:
         print(row)
--- server/src/routers/wahlkreis/wahlkreis_router.py
+++ server/src/routers/wahlkreis/wahlkreis_router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
+from database.scripts.analysis.B6.queries import *
+
 wahlkreis_router = APIRouter(
     prefix="/wahlkreis",
 )
@@ -8,3 +10,35 @@ wahlkreis_router = APIRouter(
 @wahlkreis_router.get("/")
 async def get_wahlkreis():
     return ["3asba"]
+
+@wahlkreis_router.get("/q1")
+async def query1():
+    return q1()
+
+@wahlkreis_router.get("/q2")
+async def query2():
+    return q2()
+
+@wahlkreis_router.get("/q2")
+async def query2():
+    return q2()
+
+@wahlkreis_router.get("/q3")
+async def query3():
+    return q3()
+
+@wahlkreis_router.get("/q4")
+async def query4():
+    return q4()
+
+@wahlkreis_router.get("/q5")
+async def query5():
+    return q5()
+
+@wahlkreis_router.get("/q6_winners")
+async def query6_winners():
+    return q6_winners()
+
+@wahlkreis_router.get("/q6_losers")
+async def query6_losers():
+    return q6_losers()
